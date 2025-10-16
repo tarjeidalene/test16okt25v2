541eca4d29eb02ed207fcbb0e337fe15b97480aa
tarjeidalene <173668059+tarjeidalene@users.noreply.github.com>
2025-10-16 15:51:56 +0200

    ddfewe

--- src/lib/db.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@libsql/client";
-
-export const db = createClient({
-  url: import.meta.env.ASTRO_DB_REMOTE_URL,
-  authToken: import.meta.env.ASTRO_DB_APP_TOKEN,
-});
