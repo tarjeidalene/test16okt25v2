52c3f1cd72971200a15db6e6b3bdefece1e79f1f
tarjeidalene <173668059+tarjeidalene@users.noreply.github.com>
2025-10-16 12:44:30 +0200

    Oppdatert Adapter i astro.fonfig.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 
 import db from '@astrojs/db';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db()]
+
+  integrations: [db()],
+  output: 'server',
+  adapter: netlify()
+  
 });
